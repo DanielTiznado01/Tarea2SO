1a991792b9bbcca9b6cd3a1bccb6e76b47090b07
Daniel2 <daniel.tiznado2001@alumnos.ubiobio.cl>
2023-12-02 18:33:57 -0300

    Secuencial

---
+++ Secuencial.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+// Función para convertir a escala de grises (método de luminosidad)
+Mat Grises(const Mat& colorImage) {
+    Mat grayImage(colorImage.rows, colorImage.cols, CV_8UC1);
+
+    for (int r = 0; r < colorImage.rows; ++r) {
+        for (int c = 0; c < colorImage.cols; ++c) {
+            Vec3b intensity = colorImage.at<Vec3b>(r, c);
+            uchar grayValue = intensity[0] * 0.07 + intensity[1] * 0.72 + intensity[2] * 0.21;
+            grayImage.at<uchar>(r, c) = grayValue;
+        }
+    }
+
+    return grayImage;
+}
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        cout << "Usage: ./secuencial <input_color_image> <output_gray_image>" << endl;
+        return -1;
+    }
+
+    // Cargar la imagen a color
+    Mat colorImage = imread(argv[1], IMREAD_COLOR);
+    if (colorImage.empty()) {
+        cout << "Error al cargar la imagen." << endl;
+        return -1;
+    }
+
+    // Convertir la imagen a escala de grises
+    Mat grayImage = Grises(colorImage);
+
+    // Guardar la imagen en escala de grises
+    imwrite(argv[2], grayImage);
+
+    return 0;
+    //OLI
+}
+
